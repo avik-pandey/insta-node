a3dd07196e787b35882da4b536c1643c700eb1ee
Avik Pandey <avikpandey1@gmail.com>
2020-08-02 15:41:49 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,85 @@
+const Instagram = require('instagram-web-api')
+const { username, password } = process.env
+var mongoose = require('mongoose');
+const { connected } = require('process');
+var pass = "Avik@838";
+var passENC = encodeURIComponent('Avik@838');
+console.log(passENC);
+const url = "mongodb://user:flipkart@cluster0-shard-00-00.pe7lw.mongodb.net:27017,cluster0-shard-00-01.pe7lw.mongodb.net:27017,cluster0-shard-00-02.pe7lw.mongodb.net:27017/intelFashion?ssl=true&replicaSet=atlas-r398qc-shard-0&authSource=admin&retryWrites=true&w=majority";
+// var db = 'intelFashion'; 
+console.log(url)
+mongoose.connect(url, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+
+})
+.then(() => {
+      console.log('Connection to the Atlas Cluster is successfuul!');
+  })
+.catch((err) => console.error(err));
+
+// const insta = mongoose.model('insta', { 
+//   hashtag: { type: String }, 
+//   imgUrl: { type: Number } 
+// },'insta');
+
+var instaSchema = new mongoose.Schema({
+  hashtag:  String , 
+  imgUrl: String
+  
+});
+
+
+
+var insta = mongoose.model("insta", instaSchema);
+
+const client = new Instagram({ username:'theod.ds', password:'AvikShradhaRachit' })
+var ok = {};
+var allImgLinks = [];
+client
+  .login()
+  .then(() => {
+    client
+      .getMediaFeedByHashtag({hashtag :'vogue2020'})
+      .then(   (fin)=> {
+        ok = fin['edge_hashtag_to_media'];
+        instaArr = [];
+        instaArr = ok.edges;
+        // console.log(instaArr);
+        
+        for (var i = 0;i<instaArr.length;i++){
+          // console.log(instaArr[i].node)
+          fin = {
+            hashtag : 'vogue2020',
+            imgUrl :  instaArr[i].node.display_url
+          }
+          allImgLinks.push(fin);
+        }
+        
+        // for(var i = 0;i<allImgLinks.length;i++){
+        //   console.log(allImgLinks[i]);
+        //   var instaF = new insta(allImgLinks[i]); 
+        //   insta.create(instaF, function(err, user){
+        //     if(err) console.log("err2");
+        //     else{
+        //       console.log("inserted itemm"+instaF);
+              
+        //     }
+        //    });
+          
+        // }
+        insta.deleteMany({});
+        insta.insertMany(allImgLinks).then(function(){ 
+          console.log("Data inserted") 
+          console.log(allImgLinks.length) // Success 
+      }).catch(function(error){ 
+          console.log(error)      // Failure 
+      }); 
+
+        
+
+             
+  })
+      
+  })
+  
